ce82f8370349cb6d379ac3fb628a39ffeeb07458
Diego Parra <diegod2694@gmail.com>
2018-07-16 18:13:29 -0400

    WIP

--- app/public/wp-content/themes/proyectocomercial/functions.php
+++ app/public/wp-content/themes/proyectocomercial/functions.php
@@ -3,4 +3,4 @@
     wp_enqueue_style( 'customstyle', get_template_directory_uri() . '/css/awesome.css', array(), '1.0.0', 'all');
   }
 
-  // add_action(,,,)
+  // add_action( 'wp_enqueue_scripts', 'awesome_script_', priority, accepted_args )
